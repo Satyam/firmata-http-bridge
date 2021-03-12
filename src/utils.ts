dfced27a95725a7c9faf91eaf27372026be43faa
Satyam <satyam@satyam.com.ar>
2021-03-12 15:29:20 +0100

    Added test for valid digital output

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,9 @@ export const validDigitalPin = (board: Board, pin: number) =>
 
 export const validMode = (board: Board, pin: number, mode: Board.PIN_MODE) =>
   board.pins[pin].supportedModes.includes(mode);
+
+export const validOutput = (
+  board: Board,
+  pin: number,
+  output: Board.PIN_STATE
+) => output === Board.PIN_STATE.LOW || output === Board.PIN_STATE.HIGH;
