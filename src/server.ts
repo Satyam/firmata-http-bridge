70e3837e04e143168ebd42350bd8fbedaae460d1
Satyam <satyam@satyam.com.ar>
2021-03-12 17:20:34 +0100

    provided server with `start` and `stop` functions

---
+++ src/server.ts
@@ -0,0 +1,90 @@
+import express from 'express';
+import { createServer, Server } from 'http';
+import { resolve } from 'path';
+import { config as envRead } from 'dotenv';
+
+// https://github.com/firmata/firmata.js/tree/master/packages/firmata.js
+import Board from 'firmata';
+
+import { FSA, Commands, ErrorCodes } from './types';
+import * as pinCmds from './pinCommands';
+
+const commands: Record<string, Commands> = {
+  ...pinCmds,
+};
+
+envRead();
+let http: Server;
+let board: Board;
+
+export function start() {
+  const app = express();
+  http = createServer(app);
+
+  app.use(express.json());
+
+  app.get('/version', (req, res) => {
+    res.json(board.firmware);
+  });
+
+  app.get('/AnalogPins', (req, res) => {
+    res.json(board.analogPins);
+  });
+
+  app.get('/DigitalPins/:pin?', (req, res) => {
+    const pins = board.pins;
+    if (req.params.pin) {
+      res.json(pins[parseInt(req.params.pin, 10)]);
+    } else {
+      res.json(pins.length);
+    }
+  });
+
+  app.post('/command', function (req, res) {
+    const action = req.body as FSA;
+
+    const { type } = action;
+
+    if (type in commands) {
+      res.json(commands[type](board, action));
+    } else {
+      res.json({
+        ...action,
+        error: {
+          code: ErrorCodes.BAD_ACTION_TYPE,
+          msg: 'Invalid command',
+        },
+      });
+    }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+      root: resolve(__dirname, '../build'),
+      dotfiles: 'deny',
+    });
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(req.path, {
+      root: resolve(__dirname, '../build'),
+      dotfiles: 'deny',
+    });
+  });
+
+  board = new Board(process.env.REACT_APP_USB_PORT);
+
+  board.on('ready', () => {
+    console.log('Arduino is ready to communicate');
+    http.listen(process.env.REACT_APP_HTTP_PORT, () =>
+      console.log(
+        `Example app listening on port ${process.env.REACT_APP_HTTP_PORT}!`
+      )
+    );
+  });
+}
+export function stop() {
+  console.log('Server closing');
+  http.close();
+  board.reset();
+}
