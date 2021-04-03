ce5a96d3a0316487834837305c83ce461ad447c8
Satyam <satyam@satyam.com.ar>
2021-04-03 18:08:31 +0200

    Best firmata mock I could manage

--- __mocks__/firmata.ts
+++ __mocks__/firmata.ts
@@ -1,68 +1,229 @@
 // @ts-nocheck
-import SerialPort from '@serialport/stream';
+import SerialPort from 'serialport';
+import StreamSerialPort from '@serialport/stream';
 import AbstractBinding from '@serialport/binding-abstract';
+import Board from 'firmata';
 
-let readyCallback: () => void;
-let instantiated = false;
-
-export const mockOn = jest.fn((eventName, cb) => {
-  switch (eventName) {
-    case 'ready':
-      if (instantiated) setTimeout(cb, 10);
-      else readyCallback = cb;
-      return;
-    case 'error':
-      return;
-    default:
-      console.error(`got unexpected ${eventName}`);
-      return;
+class MockSerialPortBinding extends AbstractBinding {
+  constructor(
+    path: string,
+    options: SerialPort.Options,
+    callback: SerialPort.callback
+  ) {
+    super(path, options, (error) => {
+      console.log('MockSerialPortBinding', error);
+      callback(error);
+    });
+    this.sp = new StreamSerialPort(path, options, (error) => {
+      console.log({ error });
+    });
+    console.log('constructor');
   }
-});
+}
 
-export const mockPinMode = jest.fn((pin, mode) => {
-  console.log(`pinMode, pin: ${pin}, mode: ${mode}`);
-});
-export const mockDigitalWrite = jest.fn((pin, output) => {
-  console.log(`digitalWrite, pin: ${pin}, output: ${output}`);
-});
-
-export const mockDigitalRead = jest.fn((pin, cb) => {
-  console.log(`digitalRead, pin: ${pin}`);
-  cb(1);
-});
-
-export const mockReportDigitalPin = jest.fn((pin, mode) => {
-  console.log(`reportDigitalPin, pin: ${pin}, mode: ${mode}`);
-});
-
-export const mockReset = jest.fn();
+SerialPort.Binding = MockSerialPortBinding;
 
 const mock = jest.fn((port) => {
-  SerialPort.Binding = AbstractBinding;
+  // SerialPort.list().then(
+  //   (ports) => ports.forEach(console.log),
+  //   (err) => console.error(err)
+  // );
+
+  let readyCallback: () => void;
+  let instantiated = false;
 
   const RealBoard = jest.requireActual('firmata');
-  let realBoardInstance;
-  try {
-    realBoardInstance = new RealBoard(new SerialPort(port));
-  } catch (err) {
+  let realBoardInstance: Board;
+  // try {
+  //   const b = new RealBoard(port);
+  //   console.log('returning');
+  //   return b;
+  // } catch {
+  //   console.log('No board available at', port);
+  // }
+
+  realBoardInstance = new RealBoard(new SerialPort(port), (err) => {
     console.error('caught error', err);
-  }
+  });
   instantiated = true;
   if (readyCallback) {
     setTimeout(readyCallback, 10);
   }
+  const pins = [
+    {
+      supportedModes: [],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 3, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 127,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 0,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 1,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 2,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 3,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 6, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 4,
+    },
+    {
+      supportedModes: [0, 1, 2, 4, 6, 11],
+      mode: undefined,
+      value: 0,
+      report: 1,
+      analogChannel: 5,
+    },
+  ];
   return {
     ...realBoardInstance,
-    on: mockOn,
-    pinMode: mockPinMode,
-    digitalWrite: mockDigitalWrite,
-    digitalRead: mockDigitalRead,
-    reportDigitalPin: mockReportDigitalPin,
-    reset: mockReset,
+    on: jest.fn((eventName, cb) => {
+      switch (eventName) {
+        case 'ready':
+          if (instantiated) setTimeout(cb, 10);
+          else readyCallback = cb;
+          return;
+        case 'error':
+          return;
+        default:
+          console.error(`got unexpected ${eventName}`);
+          return;
+      }
+    }),
+    pinMode: jest.fn((pin: number, mode: number) => {
+      pins[pin].mode = mode;
+    }),
+    digitalWrite: jest.fn((pin: number, output: number) => {
+      pins[pin].value = output;
+    }),
+    digitalRead: jest.fn((pin: number, cb) => {
+      cb(1);
+    }),
+    reportDigitalPin: jest.fn((pin: number, mode: number) => {
+      pins[pin].report = mode;
+    }),
+    reset: jest.fn(),
     transport: {
       isOpen: false,
     },
     isReady: true,
+    pins,
   };
 });
 
